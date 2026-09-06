727a49ee4b648d83a9b11c107b2744fee10eecfa
City_Scripts <maxjacobdiaz@gmail.com>
2026-09-06 14:35:34 +0200

    feat: add modern standalone HUD manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'maksiu07'
+description 'Modern standalone FiveM HUD'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
